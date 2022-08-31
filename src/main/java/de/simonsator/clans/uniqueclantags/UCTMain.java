f6c71825c33f6eef6a9f6d75fa3f3e1e7d46f004
Simonsator <00pflaume@gmail.com>
2022-08-31 21:43:47 +0200

    Use PoolSQLCommunication

--- src/main/java/de/simonsator/clans/uniqueclantags/UCTMain.java
+++ src/main/java/de/simonsator/clans/uniqueclantags/UCTMain.java
@@ -4,12 +4,14 @@ import de.simonsator.partyandfriends.api.PAFExtension;
 import de.simonsator.partyandfriends.clan.api.events.ClanTagSetEvent;
 import de.simonsator.partyandfriends.clan.commands.ClanCommands;
 import de.simonsator.partyandfriends.communication.sql.MySQLData;
+import de.simonsator.partyandfriends.communication.sql.pool.PoolData;
 import de.simonsator.partyandfriends.main.Main;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * @author Simonsator
@@ -22,14 +24,17 @@ public class UCTMain extends PAFExtension implements Listener {
 	@Override
 	public void onEnable() {
 		try {
+			PoolData poolData = new PoolData(Main.getInstance().getGeneralConfig().getInt("MySQL.Pool.MinPoolSize"),
+					Main.getInstance().getGeneralConfig().getInt("MySQL.Pool.MaxPoolSize"),
+					Main.getInstance().getGeneralConfig().getInt("MySQL.Pool.InitialPoolSize"), Main.getInstance().getGeneralConfig().getInt("MySQL.Pool.IdleConnectionTestPeriod"), Main.getInstance().getGeneralConfig().getBoolean("MySQL.Pool.TestConnectionOnCheckin"));
 			con = new UCTMySQLConnection(new MySQLData(Main.getInstance().getGeneralConfig().get("MySQL.Host").toString(),
 					Main.getInstance().getGeneralConfig().get("MySQL.Username").toString(), Main.getInstance().getGeneralConfig().get("MySQL.Password").toString(),
 					Main.getInstance().getGeneralConfig().getInt("MySQL.Port"), Main.getInstance().getGeneralConfig().get("MySQL.Database").toString(),
-					Main.getInstance().getGeneralConfig().get("MySQL.TablePrefix").toString(), Main.getInstance().getGeneralConfig().getBoolean("MySQL.UseSSL"), Main.getInstance().getGeneralConfig().getBoolean("MySQL.Cache")));
+					Main.getInstance().getGeneralConfig().get("MySQL.TablePrefix").toString(), Main.getInstance().getGeneralConfig().getBoolean("MySQL.UseSSL"), Main.getInstance().getGeneralConfig().getBoolean("MySQL.Cache")), poolData);
 			message = ClanCommands.getInstance().getPrefix() + (new UCTConfiguration(new File(getConfigFolder(), "config.yml"), this)).getString("Messages.ClanTagDoesAlreadyExist");
 			getAdapter().registerListener(this, this);
 			registerAsExtension();
-		} catch (IOException e) {
+		} catch (IOException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
--- src/main/java/de/simonsator/clans/uniqueclantags/UCTMySQLConnection.java
+++ src/main/java/de/simonsator/clans/uniqueclantags/UCTMySQLConnection.java
@@ -1,30 +1,33 @@
 package de.simonsator.clans.uniqueclantags;
 
 import de.simonsator.partyandfriends.communication.sql.MySQLData;
-import de.simonsator.partyandfriends.communication.sql.SQLCommunication;
+import de.simonsator.partyandfriends.communication.sql.pool.PoolData;
+import de.simonsator.partyandfriends.communication.sql.pool.PoolSQLCommunication;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @author Simonsator
  * @version 28.12.2016
  */
-public class UCTMySQLConnection extends SQLCommunication {
+public class UCTMySQLConnection extends PoolSQLCommunication {
 	private final String TABLE_PREFIX;
 
-	public UCTMySQLConnection(MySQLData pMySQLData) {
-		super(pMySQLData);
+	public UCTMySQLConnection(MySQLData pMySQLData, PoolData pPooldata) throws SQLException {
+		super(pMySQLData, pPooldata);
 		this.TABLE_PREFIX = pMySQLData.TABLE_PREFIX;
 	}
 
 	public boolean doesClanTagExist(String pClanTag) {
 		Connection con = getConnection();
 		ResultSet rs = null;
-		Statement stmt = null;
 		PreparedStatement prepStmt = null;
 		try {
 			prepStmt = con
-					.prepareStatement("select id from " + DATABASE + ".`" + TABLE_PREFIX + "clan` WHERE clan_tag=? LIMIT 1");
+					.prepareStatement("select id from `" + TABLE_PREFIX + "clan` WHERE clan_tag=? LIMIT 1");
 			prepStmt.setString(1, pClanTag);
 			rs = prepStmt.executeQuery();
 			if (rs.next())
@@ -32,7 +35,7 @@ public class UCTMySQLConnection extends SQLCommunication {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			close(rs, stmt, prepStmt);
+			close(rs, prepStmt);
 		}
 		return false;
 	}
